74cbe1f1f1b60ddf5fe220be74e376a8a99fb256
Ignacio Bona <ibonapiedrabuena@linkedin.com>
2019-08-14 15:30:47 -0700

    0.0.3

--- addon/components/medium-editor.js
+++ addon/components/medium-editor.js
@@ -50,9 +50,15 @@ export default Component.extend({
    * Use didReceiveAttrs hook to listen for props changes and update MediumEditor
    * options.
    */
-  didReceiveAttrs() {
+  init() {
     this._super(...arguments);
     this._scheduleSetupMediumEditor();
+  },
+
+  didUpdateAttrs() {
+    this._super(...arguments);
+    set(this, '_firstRender', true);
+    this._setContent();
   },
 
   /**
